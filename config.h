b38f93e5fbe07cfb64a3421172077cf67033b66f
abrabant <abrabant@student.42.fr>
2021-03-15 21:15:52 +0100

    Manual alpha support patch

--- config.h
+++ config.h
@@ -93,6 +93,9 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 4;
 
+/* bg opacity */
+float alpha = 0.85;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
@@ -119,6 +122,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#282c34", /* background */
 	"#bbc2cf", /* foreground */
+	"black",
 };
 
 /*
@@ -126,6 +130,7 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 257;
+/*unsigned int defaultbg = 256;*/
 unsigned int defaultbg = 256;
 static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 256;
